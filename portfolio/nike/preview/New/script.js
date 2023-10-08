e145a92ea30d228fec5f29f6265b6ad136f16688
Mannie1 <ikehmannie@gmail.com>
2023-10-08 23:32:25 +0100

    added bsweb under portfolio

---
+++ portfolio/nike/preview/New/script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function(){
+    // alert ("Good Afternoon")
+    x = 1;
+    document.getElementById.innerHTML = "dog";
+    let a = 40;
+    var y = 50;
+    const n = "steve";
+    const b = "20";
+    let name = false;
+    var c = true;
+    let a,b,c;
+});
+
